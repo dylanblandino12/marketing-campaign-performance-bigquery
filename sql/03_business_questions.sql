9aa763ef73a2b42431b7dc464414c65ecdc60811
dylanblandino12 <dylanbu0908@gmail.com>
2026-01-01 11:06:15 -0600

    Update 03_business_questions.sql

--- sql/03_business_questions.sql
+++ sql/03_business_questions.sql
@@ -1,1 +1,6 @@
-
+-- =====================================
+-- File: 03_business_questions.sql
+-- Purpose: Answer key business questions using SQL
+-- Dataset: Marketing Campaign Performance
+-- Tool: Google BigQuery
+-- =====================================
