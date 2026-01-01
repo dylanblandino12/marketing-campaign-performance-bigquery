083975dadc278c85f9f6e5827ac797ad2f788762
dylanblandino12 <dylanbu0908@gmail.com>
2026-01-01 17:10:08 -0600

    Update 03_business_questions.sql

--- sql/03_business_questions.sql
+++ sql/03_business_questions.sql
@@ -8,3 +8,14 @@
 -- Q1: Which marketing channel delivers the highest average ROI?
 
 
+SELECT
+  Channel_Used,
+  ROUND(AVG(ROI),2) AS avg_roi
+FROM `marketing_campaign_performance.raw_campaign_data`
+GROUP BY Channel_Used
+ORDER BY avg_roi DESC
+LIMIT 1;
+
+-- Insight: Facebook delivers the highest average ROI among all channels, indicating stronger return efficiency compared to other platforms.
+
+
